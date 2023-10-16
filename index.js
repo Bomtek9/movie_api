4b11b9c954ef44e0bcc16adb2141f207dcddb316
Bomtek9 <clayduplantis9@gmail.com>
2023-10-16 12:28:36 -0500

    Auth jwt token

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.get("/documentation", (req, res) => {
 // All movies
 app.get(
   "/movies",
-  //   passport.authenticate("jwt", { session: false }),
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     await Movies.find()
       .then((movies) => {
